c0b2e8e5146b421a8dd5e92040272c3e7cce5f0d
Noel Zamora <noelio1290@gmail.com>
2023-01-31 12:48:59 -0600

    funciones para obtener info de pokemon desde el server funcionando, falta solisitud de fronted y renderizar pokemons

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const port = 8000;
 
@@ -6,3 +6,34 @@ app.listen(port, function(){
     console.log(`Inicializado en el puerto ${port}`);
     console.log(`http://localhost:${port}`);
 });
+
+/*function fetchPokemon(id){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then((res)=>res.json)
+    .then((data)=>console.log(data))
+};*/
+
+const bringPokemon = async (id)=>{
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    const data = await res.json()
+    console.log(data.name)
+};
+
+const bringPokemons =(number)=>{
+    for(let i = 1; i <= number; i++){
+        bringPokemon(i)
+    }
+};
+
+//fetchPokemon(1)
+//bringPokemon(1)
+//bringPokemon(2)
+//bringPokemon(3)
+//bringPokemon(4)
+//bringPokemon(5)
+//bringPokemon(6)
+//bringPokemon(7)
+//bringPokemon(8)
+//bringPokemon(9)
+//bringPokemon(10)
+bringPokemons(10)
