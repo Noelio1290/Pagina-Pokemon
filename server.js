646fc76ef3662f3c939a4a797242ed02f0d7710f
Noel Zamora <noelio1290@gmail.com>
2023-01-31 17:15:46 -0600

    funciones para obtenr info de pokemon desde api direto al server,propiedades del pokemon(name,id,imagen)

--- server.js
+++ server.js
@@ -1,39 +1,43 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 8080;
+const url = "https://pokeapi.co/api/v2/pokemon"
+
+//functions
+const bringPokemon = async (id)=>{
+    const res = await fetch(`${url}/${id}`)
+    const data = await res.json()
+    const datosPokemon = {
+        id:data.id,
+        name:data.name,
+        imagen:data.sprites.front_default
+    }
+    return datosPokemon
+};
+
+const bringPokemons =async (number)=>{
+    const pokeNames = []
+    for(let i = 1; i <= number; i++){
+        pokeNames.push(await bringPokemon(i))
+    }
+    return pokeNames
+};
+
+const getUserInfo = async (request, response)=>{
+    const data = await bringPokemons(20);
+    response.setHeader("Content-Type", "text/html");
+    response.send(data);
+    response.end();
+};
+
+//Manejar las peticiones
+//app.use(respuesta)
+app.use(express.json());
+app.use('/public', express.static('paginaPokemon'));
+
+app.get('/', getUserInfo); //Trae algo
 
 app.listen(port, function(){
     console.log(`Inicializado en el puerto ${port}`);
     console.log(`http://localhost:${port}`);
 });
-
-/*function fetchPokemon(id){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    .then((res)=>res.json)
-    .then((data)=>console.log(data))
-};*/
-
-const bringPokemon = async (id)=>{
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-    const data = await res.json()
-    console.log(data.name)
-};
-
-const bringPokemons =(number)=>{
-    for(let i = 1; i <= number; i++){
-        bringPokemon(i)
-    }
-};
-
-//fetchPokemon(1)
-//bringPokemon(1)
-//bringPokemon(2)
-//bringPokemon(3)
-//bringPokemon(4)
-//bringPokemon(5)
-//bringPokemon(6)
-//bringPokemon(7)
-//bringPokemon(8)
-//bringPokemon(9)
-//bringPokemon(10)
-bringPokemons(10)
